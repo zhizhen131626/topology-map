ffe488ee3aa83165b533fea4dc6eb3dedf3213d8
huangzhizhen <huangzhizhen@comtom.cn>
2024-02-04 15:42:31 +0800

    feat: 创建

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <topology-map msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TopologyMap from './components/TopologyMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopologyMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TopologyMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="topology-map">
+    拓扑图
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TopologyMap',
+  props: {
+    msg: String
+  },
+  components: { OfflineMap },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.topology-map {
+  width: 1920px;
+  height: 100vh;
+}
+</style>
